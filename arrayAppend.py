c1b217258c04d54daad87b00f878667f02e2453e
SilverAzoth <osoriocm99@gmail.com>
2021-06-15 16:58:15 -0600

    added array and blackboard files

---
+++ arrayAppend.py
@@ -0,0 +1,4 @@
+array = []
+userInput = input("Add to the array! ")
+array.append(userInput)
+print(array[0])
---
+++ loopingArrayAppend.py
@@ -0,0 +1,8 @@
+array = []
+loop = 0
+
+while loop < 3:
+    userInput = input("Add to the array! ")
+    array.append(userInput)
+    print(array)
+    loop +=1
